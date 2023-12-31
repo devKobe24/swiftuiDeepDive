3d095003d284b250ebc4c0397591eac978d0e614
kobe <dev.skyachieve91@gmail.com>
2024-01-01 08:41:52 +0900

    :sparkles: Feat: create Chat Field and Send Button.

--- Messenger/Messenger/Views/ChatView.swift
+++ Messenger/Messenger/Views/ChatView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ChatView: View {
+	@State var message: String = ""
+	
     var body: some View {
-			ScrollView(.vertical) {
-				ChatRow(type: .sent)
-					.padding(3)
-				ChatRow(type: .received)
-					.padding(3)
+			VStack {
+				ScrollView(.vertical) {
+					ChatRow(type: .sent)
+						.padding(3)
+					ChatRow(type: .received)
+						.padding(3)
+				}
+				// Field, send button
+				HStack {
+					TextField("Message...", text: $message)
+						.padding()
+						.background(Color(.secondarySystemBackground))
+						.cornerRadius(7)
+					
+					SendButton(text: $message)
+				}
+				.padding()
 			}
+			.navigationTitle("RubberDuck")
     }
 }
 
--- Messenger/Messenger/Views/SendButton.swift
+++ Messenger/Messenger/Views/SendButton.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct SendButton: View {
+	@Binding var text: String
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+			Button(action: {
+				self.sendMessage()
+			}, label: {
+				Image(systemName: "paperplane")
+					.font(.system(size: 33))
+					.aspectRatio(contentMode: .fit)
+					.frame(width: 55, height: 55)
+					.foregroundColor(.white)
+					.background(.blue)
+					.clipShape(Circle())
+			})
     }
+	
+	func sendMessage() {
+		guard !text.isEmpty else {
+			return
+		}
+	}
 }
 
 #Preview {
